fec6de1248b31efe7e651fab1f64523af62487fb
max-navsegda <max-navsegda@mail.ru>
2020-08-24 17:41:45 +0600

    adjusted MySQL connector

--- complete/src/main/java/com/example/accessingdatamysql/MainController.java
+++ complete/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -30,7 +30,7 @@ public class MainController {
 
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
-		// This returns a JSON or XML with the users.
+		// This returns a JSON or XML with the users
 		return userRepository.findAll();
 	}
 }
